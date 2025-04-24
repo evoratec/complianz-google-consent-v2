e2c43359668a1bdf93cac411ff8d3f347cbde3f2
Juan Antonio Navarro <evoratec@gmail.com>
2025-04-25 01:24:21 +0200

    Primer commit
    Primer commit

---
+++ consentmode-complianz/consentmode-complianz.php
@@ -0,0 +1,18 @@
+<?php
+/*
+/*
+Plugin Name:  Complianz Google Consent Mode v2 para
+Description: Integra el Consent Mode v2 de Google con el plugin Complianz.
+Version: 1.0
+Author: Evoratec
+Author URI: https://evoratec.com
+License: GPL2
+*/
+
+
+add_action('wp_enqueue_scripts', 'consentmode_complianz_script');
+function consentmode_complianz_script() {
+    wp_enqueue_script('consentmode-complianz', plugin_dir_url(__FILE__) . 'consentmode-complianz.js', array(), null, true);
+}
+
+?>
